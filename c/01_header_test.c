bc824550b7a355755a423490e3e3d4552c5105f1
Liemani <parkjh2768@naver.com>
2021-04-23 17:33:49 +0900

    backup

--- c/01_header_test.c
+++ c/01_header_test.c
@@ -1,11 +1,12 @@
-// #include <stddef.h>
-// #include <errno.h> // errno
-// #include <sys/types.h>
-// #include <sys/uio.h>
-// #include <stdarg.h>
-// #include <wchar.h>
-// #include <stdbool.h>
-#include <stdlib.h>
+//#include <stddef.h>
+//#include <errno.h> // errno
+//#include <sys/types.h>
+//#include <sys/uio.h>
+//#include <stdarg.h>
+//#include <wchar.h>
+//#include <stdbool.h>
+//#include <stdlib.h>
+#include <math.h>
 
 int main(void)
 {
