db4b5b35b9bff67d3583c8bca3bf1d287aa80823
Liemani <parkjh2768@naver.com>
2021-05-26 02:25:36 +0900

    backup

--- Push_swap/t_queue.c
+++
@@ -1,8 +0,0 @@
-#include "t_queue.h"
-
-void	init_queue(t_queue *p_queue, int *p_array, int front_index, int rear_index)
-{
-	p_queue->p_array = p_array;
-	p_queue->front_index = front_index;
-	p_queue->rear_index = rear_index;
-}
