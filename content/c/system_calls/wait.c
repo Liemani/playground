b9b8a56b1b747715d48d1f2c61fee96264915187
Liemani <parkjh2768@naver.com>
2021-10-20 16:24:24 +0900

    wait.c/008

--- content/c/system_calls/wait.c
+++ content/c/system_calls/wait.c
@@ -8,31 +8,38 @@
 
 
 
-///	- What if there is several child rather than only 1?
-///	- Would the order of calling exit() between child be important of the operating of wait()?
-
-///	- I called fork() twice, and wait() twice also.
-///	- result:
-///		pid1: [9277].
-///		pid2: [9278].
-///		This is child1 process.
-///		This is child2 process.
-///		pid waited: [9277].
-///		stat_loc: [512].
-///		pid waited: [9278].
-///		stat_loc: [768].
-///	- We can earn exit code from stat_loc.
-///		- exit_code = (stat_loc >> 8) & 0x000000ff;
-///		- So the value of stat_loc set by wait() is normal.
-
 ///	Now let child1 sleep for a short time to exit after child2.
 /// And I will put sleep to parent also.
 ///	If there is no sleep in parent, it wait child2 first, and wait child1 second.
 /// There is no things to think.
 ///	I want to see child2 exit, child1 exit, and parent wait.
 
+///	- result:
+///		This is child2 process.
+///		This is child1 process.
+///		pid1: [10664].
+///		pid2: [10665].
+///		pid waited: [10665].
+///		stat_loc: [768].
+///		pid waited: [10664].
+///		stat_loc: [512].
+///	- parent's first wait() returned pid of child2.
+///	- We can assume that when child process call exit(),
+///		the status is saved on queue buffer.
+///	- So when we request the data by calling wait(),
+///		the first exited process data is returned.
+
+///	- But it is difficult to be certain that
+///		all wait() work like queue structure.
+///		- If the man page doesn't mention about that feature,
+///			that can not be a common behavior.
+///		- For this reason, it is best to avoid using such features undescribed on documentation.
+
+///	- Now let's dig into the wait4() function.
+
 void	child1()
 {
+	sleep(1);
 	printf("This is child1 process. \n");
 	exit(2);
 }
@@ -48,6 +55,7 @@ void	parent(pid_t pid1, pid_t pid2)
 	pid_t	wait_pid;
 	int		stat_loc;
 
+	sleep(1);
 	printf("pid1: [%d]. \n", pid1);
 	printf("pid2: [%d]. \n", pid2);
 
