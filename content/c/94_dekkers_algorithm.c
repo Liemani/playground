ecf6c78c565c884005626390e9a55fd9202147da
Liemani <parkjh2768@naver.com>
2021-12-10 19:38:19 +0900

    94_dekkers_algorithm.c/002

--- content/c/94_dekkers_algorithm.c
+++ content/c/94_dekkers_algorithm.c
@@ -1,10 +1,6 @@
 // link -> https://en.wikipedia.org/wiki/Dekker%27s_algorithm#/media/File:Dekker's_Algorithm.svg
-// Now, let's apply dekker's algorithm
-// This commit i analyze dekker's algorithm on my mind
-// Initial turn order must defined
-// And turn must be given sequentially
-// I thought that if remove wants_to_enter[], it would be compacted
-// But as a result, it is caused the possibility of deadlock and starvation
+// Now refactoring code base psudo code on the link
+// dekker algorithm work only for variable shared between exactly 2 thread
 #include <pthread.h>	// phtread_
 #include <stddef.h>	// NULL
 #include <stdio.h>	// printf()
@@ -20,16 +16,16 @@
 typedef struct s_thread_res
 {
 	pthread_t	tid;
+	int			tindex;
 	int			*shared_data;
 	int			*current_turn;
-	int			*last_turn;
-	int			my_turn;
+	int			*wants_to_enter;
 }	t_thread_res;
 
 static void	thread_res_init(t_thread_res *tres, int *shared_data,
-		int *current_turn, int *last_turn);
+		int *current_turn, int *wants_to_enter);
 static void	*thread_run(void *arg);
-static void	want_to_enter(t_thread_res *tres,
+static void	dekker_algorithm(t_thread_res *tres,
 		int *shared_data,
 		int *before_shared_data,
 		int *computed_data);
@@ -42,18 +38,19 @@ int	main(void)
 {
 	int				shared_data;
 	int				current_turn;
-	int				last_turn;
+	int				wants_to_enter[2];
 	t_thread_res	tres1;
 	t_thread_res	tres2;
 
 	shared_data = 0;
 	current_turn = 0;
-	last_turn = 2;
+	wants_to_enter[0] = FALSE;
+	wants_to_enter[0] = FALSE;
 
-	tres1.my_turn = 0;
-	tres2.my_turn = 1;
-	thread_res_init(&tres1, &shared_data, &current_turn, &last_turn);
-	thread_res_init(&tres2, &shared_data, &current_turn, &last_turn);
+	tres1.tindex = 0;
+	tres2.tindex = 1;
+	thread_res_init(&tres1, &shared_data, &current_turn, wants_to_enter);
+	thread_res_init(&tres2, &shared_data, &current_turn, wants_to_enter);
 
 	pthread_create(&tres1.tid, NULL, thread_run, &tres1);
 	pthread_create(&tres2.tid, NULL, thread_run, &tres2);
@@ -65,11 +62,11 @@ int	main(void)
 }
 
 static void	thread_res_init(t_thread_res *tres, int *shared_data,
-		int *current_turn, int *last_turn)
+		int *current_turn, int *wants_to_enter)
 {
 	tres->shared_data = shared_data;
 	tres->current_turn = current_turn;
-	tres->last_turn = last_turn;
+	tres->wants_to_enter = wants_to_enter;
 }
 
 static void	*thread_run(void *arg)
@@ -84,29 +81,39 @@ static void	*thread_run(void *arg)
 	shared_data = tres->shared_data;
 	for (index = 0; index < LOOP_COUNT; ++index)
 	{
-		want_to_enter(tres, shared_data, &before_shared_data, &computed_data);
+		dekker_algorithm(tres, shared_data, &before_shared_data, &computed_data);
 		do_some_job();
 	}
 	return (NULL);
 }
 
-static void	want_to_enter(t_thread_res *tres,
+static void	dekker_algorithm(t_thread_res *tres,
 		int *shared_data,
 		int *before_shared_data,
 		int *computed_data)
 {
-	while (tres->my_turn != *tres->current_turn)
-		;
+	const int	tindex = tres->tindex;
+	int	*const	wants_to_enter = tres->wants_to_enter;
+	int	*const	current_turn = tres->current_turn;
+
+	wants_to_enter[tindex] = TRUE;
+	while (wants_to_enter[!tindex])
+	{
+		if (*current_turn != tindex)
+			wants_to_enter[tindex] = FALSE;
+		while (*current_turn != tindex)
+			;
+		wants_to_enter[tindex] = TRUE;
+	}
 
 	*before_shared_data = *shared_data;
 	*computed_data = plus_one(*before_shared_data);
-	printf("shared_data: before[%3d], computed[%3d], after[%3d] \n",
-			*before_shared_data, *computed_data, *shared_data);
+	printf("tindex: %d, shared_data: before[%3d], computed[%3d], after[%3d] \n",
+			tindex, *before_shared_data, *computed_data, *shared_data);
 	*shared_data = *computed_data;
 
-	tres->my_turn = *tres->last_turn;
-	++*tres->last_turn;
-	++*tres->current_turn;
+	*current_turn = !*current_turn;
+	wants_to_enter[tindex] = FALSE;
 }
 
 static int	plus_one(int number)
